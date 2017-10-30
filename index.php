afda0cdf42075de426fec8e7a39af511f59efb18
Dimitri PILARSKI <dpilarsk@student.42.fr>
2017-10-30 19:36:36 +0100

    try nav

--- index.php
+++ index.php
@@ -12,12 +12,20 @@
 	<meta charset="UTF-8">
 	<title>Camagru</title>
 	<link rel="stylesheet" type="text/css" href="resources/css/style.css">
+	<meta name=""viewport content="width=device-width"/>
 </head>
 <body>
-	<header>
-		<nav>
-			test
-		</nav>
-	</header>
+<header>
+	<nav>
+		<ul>
+			<li><a href="#">Accueil</a></li>
+			<li><a href="#">Galerie</a></li>
+			<li><a href="#">Connexion</a></li>
+			<li><a href="#">Inscription</a></li>
+			<li><a href="#">Se deconnecter</a></li>
+			<li><a href="#">Mon compte</a></li>
+		</ul>
+	</nav>
+</header>
 </body>
 </html>
