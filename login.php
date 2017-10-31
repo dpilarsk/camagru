05a88c79b635773c20e7e7fe1448108958b2d008
Dimitri PILARSKI <dpilarsk@student.42.fr>
2017-10-31 19:16:58 +0100

    change button

--- login.php
+++ login.php
@@ -1,7 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dpilarsk
- * Date: 10/31/17
- * Time: 6:53 PM
- */
+require_once 'Autoloader.php';
+Autoloader::register();
+require_once 'config/database.php';
+$db = new Database($DB_DSN, $DB_USER, $DB_PASSWORD);
+$db->connect("camagru");
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Camagru - Inscription</title>
+	<link rel="stylesheet" type="text/css" href="resources/css/style.css">
+	<meta name=""viewport content="width=device-width"/>
+</head>
+<body>
+<?php include_once 'resources/partials/header.php'; ?>
+<br>
+<div class="container">
+	<form action="/login.php" method="POST">
+		<input type="text" placeholder="Nom d'utilisateur" name="username" id="username">
+		<input type="password" placeholder="Mot de passe" name="password" id="password">
+		<input type="submit" id="submit" value="Connexion">
+	</form>
+</div>
+<?php include_once 'resources/partials/footer.php'?>
+<script src="resources/js/register.js"></script>
+</body>
+</html>
