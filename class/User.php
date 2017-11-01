06092686c194dafbd730fd44c4c4ccf39b49502e
Dimitri PILARSKI <dpilarsk@student.42.fr>
2017-11-01 21:04:16 +0100

    add confirm email function

--- class/User.php
+++ class/User.php
@@ -23,6 +23,7 @@ class User
 		$get_user = $this->db->prepare("SELECT * FROM users WHERE login = :username OR email = :email;");
 		$get_user->execute(array(':username' => $this->username, ':email' => $this->email));
 		$res = $get_user->fetchAll();
+		$get_user->closeCursor();
 		if (count($res) != 0)
 		{
 			echo "Un utilisateur utilise deja ce nom ou cet email !";
@@ -35,7 +36,32 @@ class User
 									':password' => $this->password,
 									':email' => $this->email,
 									':token' => $this->token));
+			mail($this->email,
+				'Confirmation du compte',
+				"Afin de confirmer votre compte Camagru, merci de cliquer ici:\n\n
+							http://localhost:8080/confirm.php?login=$this->username&token=$this->token");
 			echo "Veuillez consulter votre email pour confirmer votre compte !";
+		}
+	}
+
+	public function checkToken($login, $token)
+	{
+		$getUser = $this->db->prepare("SELECT * FROM users WHERE login = :login AND token = :token AND status = 0;");
+		$getUser->execute(array(':login' => $login,
+								':token' => $token));
+		$res = $getUser->fetchAll();
+		$getUser->closeCursor();
+		if (count($res) == 0)
+		{
+			echo "Votre compte est soit deja valide soit une de vos informations est invalide, veuillez contacter le webmaster !";
+			die();
+		}
+		else
+		{
+			$update = $this->db->prepare("UPDATE users SET status = 1 WHERE login = :login AND token = :token;");
+			$update->execute(array( ':login' => $login,
+									':token' => $token));
+			header('Location: index.php');
 		}
 	}
 }
