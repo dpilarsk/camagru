8f9fadaf131988e1181f71e0788cdbbdeeee4592
Dimitri PILARSKI <dpilarsk@student.42.fr>
2017-11-01 16:59:16 +0100

    up a little the title

--- gallery.php
+++ gallery.php
@@ -14,49 +14,62 @@ $db->connect("camagru");
 	<meta name=""viewport content="width=device-width"/>
 </head>
 <body>
-<?php include_once 'resources/partials/header.php'; ?>
-<br>
-<div class="container">
-<!--	<a href="view.php?id=#"><span class="card"></span></a>-->
-	<div class="gallery">
-		<div class="card">
-			<a href="#"><img src="http://lorempicsum.com/rio/255/200/2" alt=""></a>
-			<title class="user">User</title>
-		</div>
-		<div class="card">
-			<a href="#"><img src="http://lorempicsum.com/futurama/255/200/5" alt=""></a>
-			<title class="user">User</title>
-		</div>
-		<div class="card">
-			<a href="#"><img src="http://lorempicsum.com/futurama/255/200/2" alt=""></a>
-			<title class="user">User</title>
-		</div>
-		<div class="card">
-			<a href="#"><img src="http://lorempicsum.com/nemo/255/200/2" alt=""></a>
-			<title class="user">User</title>
-		</div>
-		<div class="card">
-			<a href="#"><img src="http://lorempicsum.com/nemo/255/200/5" alt=""></a>
-			<title class="user">User</title>
-		</div>
-		<div class="card">
-			<a href="#"><img src="http://lorempicsum.com/simpsons/255/200/2" alt=""></a>
-			<title class="user">User</title>
-		</div>
-		<div class="card">
-			<a href="#"><img src="http://lorempicsum.com/simpsons/255/200/5" alt=""></a>
-			<title class="user">User</title>
-		</div>
-		<div class="card">
-			<a href="#"><img src="http://lorempicsum.com/up/255/200/2" alt=""></a>
-			<title class="user">User</title>
-		</div>
-		<div class="card">
-			<a href="#"><img src="http://lorempicsum.com/up/350/200/1" alt=""></a>
-			<title class="user">User</title>
+	<?php include_once 'resources/partials/header.php'; ?>
+	<br>
+	<div class="container">
+	<!--	<a href="view.php?id=#"><span class="card"></span></a>-->
+		<div class="gallery">
+			<div class="card">
+				<a href="#"><img src="http://lorempicsum.com/rio/255/200/2" alt=""></a>
+				<title class="user">User</title>
+			</div>
+			<div class="card">
+				<a href="#"><img src="http://lorempicsum.com/futurama/255/200/5" alt=""></a>
+				<title class="user">User</title>
+			</div>
+			<div class="card">
+				<a href="#"><img src="http://lorempicsum.com/futurama/255/200/2" alt=""></a>
+				<title class="user">User</title>
+			</div>
+			<div class="card">
+				<a href="#"><img src="http://lorempicsum.com/nemo/255/200/2" alt=""></a>
+				<title class="user">User</title>
+			</div>
+			<div class="card">
+				<a href="#"><img src="http://lorempicsum.com/nemo/255/200/5" alt=""></a>
+				<title class="user">User</title>
+			</div>
+			<div class="card">
+				<a href="#"><img src="http://lorempicsum.com/simpsons/255/200/2" alt=""></a>
+				<title class="user">User</title>
+			</div>
+			<div class="card">
+				<a href="#"><img src="http://lorempicsum.com/simpsons/255/200/5" alt=""></a>
+				<title class="user">User</title>
+			</div>
+			<div class="card">
+				<a href="#"><img src="http://lorempicsum.com/up/255/200/2" alt=""></a>
+				<title class="user">User</title>
+			</div>
+			<div class="card">
+				<a href="#"><img src="http://lorempicsum.com/up/350/200/1" alt=""></a>
+				<title class="user">User</title>
+			</div>
 		</div>
 	</div>
-</div>
-<?php include_once 'resources/partials/footer.php'?>
+	<script>
+		function getRandomColor() {
+			var length = 6;
+			var chars = '0123456789ABCDEF';
+			var hex = '#';
+			while(length--) hex += chars[(Math.random() * 16) | 0];
+			return hex;
+		}
+		var titles = document.getElementsByClassName("user")
+		Array.prototype.forEach.call(titles, function (e) {
+			e.style.background = getRandomColor()
+		})
+	</script>
+	<?php include_once 'resources/partials/footer.php'?>
 </body>
 </html>
