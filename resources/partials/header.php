96b85d41f8a83ecbd5ca49a92b78b19d4dea6422
Dimitri PILARSKI <dpilarsk@student.42.fr>
2017-11-02 15:12:39 +0100

    add checking conenction for the header

--- resources/partials/header.php
+++ resources/partials/header.php
@@ -4,16 +4,19 @@
 			<hr>
 			<a href="/"><li>Accueil</li></a>
 			<hr>
-			<a href="gallery.php"><li>Galerie</li></a>
-			<hr>
-			<a href="login.php"><li>Connexion</li></a>
-			<hr>
-			<a href="register.php"><li>Inscription</li></a>
-			<hr>
-			<a href="#"><li>Compte</li></a>
-			<hr>
-			<a href="disconnect.php"><li>Deconnexion</li></a>
-			<hr>
+			<?php if (isset($_SESSION['id'])){ ?>
+				<a href="gallery.php"><li>Galerie</li></a>
+				<hr>
+				<a href="#"><li>Compte</li></a>
+				<hr>
+				<a href="disconnect.php"><li>Deconnexion</li></a>
+				<hr>
+			<?php } else { ?>
+				<a href="login.php"><li>Connexion</li></a>
+				<hr>
+				<a href="register.php"><li>Inscription</li></a>
+				<hr>
+			<?php } ?>
 		</ul>
 	</nav>
 </header>
