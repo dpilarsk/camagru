8d2f4c5cdbad0bdf90049b0499d3a64158c963d5
Dimitri PILARSKI <dpilarsk@student.42.fr>
2018-01-24 16:22:28 +0100

    change rotues

--- config/setup.php
+++ config/setup.php
@@ -1,8 +1,14 @@
 <?php
-require_once '../Autoloader.php';
-Autoloader::register();
-require_once './database.php';
+if (PHP_SAPI === "cli")
+{
+	require_once 'class/Database.class.php';
+	require_once 'Database.class.php';
 
-$db = new Database($DB_DSN, $DB_USER, $DB_PASSWORD);
-$db->init_database("camagru");
+	$db = new Database($DB_DSN, $DB_USER, $DB_PASSWORD);
+	$db->init_database("camagru");
+}
+else
+{
+	echo "<img src='https://media.giphy.com/media/RX3vhj311HKLe/giphy.gif'>";
+}
 ?>
